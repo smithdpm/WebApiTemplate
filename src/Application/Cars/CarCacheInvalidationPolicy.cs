e96a08d5b44ab2b32f094fd7701e38c659e4572e
Damien Smith <smithdp@tcd.ie>
2025-09-15 21:53:18 +0100

    Example of Cache Invalidation Policy on an Aggregate Add Cache Invalidation Policy for Car Aggregate

---
+++ src/Application/Cars/CarCacheInvalidationPolicy.cs
@@ -0,0 +1,13 @@
+﻿
+using Application.Behaviours.RepositoryCaching;
+using Application.Cars.GetByMake;
+using Domain.Cars;
+
+namespace Application.Cars;
+internal class CarCacheInvalidationPolicy : StandardCacheInvalidationPolicy<Car, Guid>
+{
+    protected override IEnumerable<Func<Car, string?>> CacheInvalidationFunctionsByEntity()
+    {
+        yield return (entity) => new GetCarDtosByMakeSpec(entity.Make).CacheKey;
+    }
+}
